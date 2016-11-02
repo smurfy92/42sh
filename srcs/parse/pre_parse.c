84e82c6542559d5eb614ad2e693b6576a3754b61
Smurfy <tranchierjules@gmail.com>
2016-11-02 18:52:20 +0100

    clean

--- srcs/parse/pre_parse.c
+++ srcs/parse/pre_parse.c
@@ -95,11 +95,25 @@ static void		ft_escape_parse(t_group *grp, int i)
 void		ft_pre_parse(t_group *grp)
 {
 	int i;
+	int test;
+	int ret;
 
 	i = -1;
+	test = 0;
 	while (TERM(cmd_line) && TERM(cmd_line)[++i])
 	{
-		if (!check_parentheses('\\') && TERM(cmd_line)[i] == '\\' &&
+		ret = check_parentheses(TERM(cmd_line)[i]);
+		if (test == 0 && ret == 1)
+		{
+			test = 1;
+			ft_escape_parse(grp, i - 1);
+		}
+		else if (test == 1 && ret == 0)
+		{
+			test = 0;
+			ft_escape_parse(grp, i - 1);
+		}
+		if (TERM(cmd_line)[i] == '\\' &&
 			TERM(cmd_line)[i + 1] && TERM(cmd_line)[i + 1] == '\\')
 			ft_escape_parse(grp, i);
 		if (i > 0 && TERM(cmd_line)[i] == '\\' &&
