73834102b313ce54c6a2bbf3c9786bd85d7c9372
Smurfy <tranchierjules@gmail.com>
2016-11-08 19:04:13 +0100

    termcaps multilignes

--- srcs/termcaps/keyboard_tools2.c
+++ srcs/termcaps/keyboard_tools2.c
@@ -53,7 +53,11 @@ void	ft_place_to_eof(t_group *grp)
 	i = TERM(window->width);
 	while (--i > 0)
 		tputs(tgetstr("le", NULL), 0, ft_getchar);
-
+	i = 2;
+	while (TERM(cmd_line)[TERM(curs_pos) - i - 1] != '\n' && TERM(curs_pos) + START_POS - i != 0)
+		i++;
+	while (--i > 0)
+		tputs(tgetstr("nd", NULL), 0, ft_getchar);
 }
 
 void	ft_left_arrow(t_group *grp)
@@ -78,7 +82,7 @@ void	ft_left_arrow(t_group *grp)
 void	ft_right_arrow(t_group *grp)
 {
 	TERM(curs_pos) += 1;
-	if (((TERM(curs_pos) + START_POS) % TERM(window->width)) == 0)
+	if (((TERM(curs_pos) + START_POS) % TERM(window->width)) == 0 || TERM(cmd_line)[TERM(curs_pos) - 1]  == '\n') 
 		tputs(tgetstr("do", NULL), 0, ft_getchar);
 	else
 		tputs(tgetstr("nd", NULL), 0, ft_getchar);
