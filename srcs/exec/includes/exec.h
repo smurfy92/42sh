bcf4d44532f91bbd69faf92f54de6202e9e061f9
Julien MONTIJANO <jmontija@e2r12p15.42.fr>
2016-11-12 19:57:55 +0100

    execve/check_cmd

--- srcs/exec/includes/exec.h
+++ srcs/exec/includes/exec.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jtranchi <jtranchi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jmontija <jmontija@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/25 15:42:48 by jtranchi          #+#    #+#             */
-/*   Updated: 2016/11/04 13:30:06 by jtranchi         ###   ########.fr       */
+/*   Updated: 2016/11/12 19:51:08 by jmontija         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 # define EXEC_H
 
 void		init_exec(t_group *grp);
+void		exec_child(t_group *grp, t_parse *parse);
+void		ft_fork_pipe(t_group *grp);
 
 #endif
