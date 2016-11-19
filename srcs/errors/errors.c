9fd21eaf9dc7f37a6bef788bed42366aa32bf53e
Smurfy <tranchierjules@gmail.com>
2016-11-19 15:09:34 +0100

    erros better format sorry sir

--- srcs/errors/errors.c
+++ srcs/errors/errors.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   errors.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vdanain <vdanain@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jtranchi <jtranchi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/02 12:56:35 by jtranchi          #+#    #+#             */
-/*   Updated: 2016/11/18 21:25:14 by vdanain          ###   ########.fr       */
+/*   Updated: 2016/11/19 15:09:02 by jtranchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ void	error_cmd(char *what, char *who, int exit_code)
 
 	grp = get_grp();
 	ft_putstr_fd(what, 2);
-	ft_putstr_fd(" -> ", 2);
 	ft_putendl_fd(who, 2);
 	grp->exit = exit_code;
 }
