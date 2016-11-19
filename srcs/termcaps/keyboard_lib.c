06d3f815f12f2adb38e8d2994a3c1ba92c3d18fe
Victor DANAIN <vdanain@e3r12p9.42.fr>
2016-11-19 21:47:09 +0100

    OK

--- srcs/termcaps/keyboard_lib.c
+++ srcs/termcaps/keyboard_lib.c
@@ -6,7 +6,7 @@
 /*   By: vdanain <vdanain@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/02 15:43:58 by julio             #+#    #+#             */
-/*   Updated: 2016/11/19 19:09:56 by vdanain          ###   ########.fr       */
+/*   Updated: 2016/11/19 21:42:31 by vdanain          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,17 @@ void		ft_go_down(t_group *grp)
 	i = -1;
 	while (++i < TERM(window->width) && TERM(curs_pos) < TERM(cmd_size))
 		ft_right_arrow(grp);
+}
+
+int		ft_is_printable(char *order)
+{
+	int	i;
+
+	i = -1;
+	while (order[++i] != '\0')
+		if (ft_isprint(order[i]) == false)
+			return (0);
+	return (1);
 }
 
 /*
