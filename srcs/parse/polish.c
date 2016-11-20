01b1244e277ba1ff8751a42bdab2a6c98d9e67a0
Julien MONTIJANO <jmontija@e3r1p9.42.fr>
2016-11-20 23:45:36 +0100

    exit/polish

--- srcs/parse/polish.c
+++ srcs/parse/polish.c
@@ -6,7 +6,7 @@
 /*   By: jmontija <jmontija@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/19 22:41:30 by jmontija          #+#    #+#             */
-/*   Updated: 2016/11/20 21:43:48 by jmontija         ###   ########.fr       */
+/*   Updated: 2016/11/20 22:40:21 by jmontija         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,10 @@ void		ft_polish_parse(t_parse *parse, int j, int i)
 {
 	parse->cmdsplit[j][i + 1] = '\0';
 	if (parse->cmdsplit[j][i + 2])
-		parse->cmdsplit[j] = JOINF(parse->cmdsplit[j], SDUP(&parse->cmdsplit[j][i + 2]), 3);
+	{
+		parse->cmdsplit[j] = 
+		JOINF(parse->cmdsplit[j], SDUP(&parse->cmdsplit[j][i + 2]), 3);
+	}
 }
 
 void		polisher(t_parse *parse, int i, int j, int ret, int *in)
@@ -58,13 +61,18 @@ void		polish(t_parse *parse)
 	in = 0;
 	j = -1;
 	check_parentheses(0);
+	printf("CMD: %s\n", parse->cmd);
 	while (parse->cmdsplit && parse->cmdsplit[++j])
-	{	
+	{
 		i = -1;
 		while (parse->cmdsplit[j][++i])
 		{
 			ret = check_parentheses(parse->cmdsplit[j][i]);
-			if (parse->cmdsplit[j][i] == '\\' && parse->cmdsplit[j][i + 1])
+			if (ret == 0 && parse->cmdsplit[j][i] == '\\' &&
+				parse->cmdsplit[j][i + 1])
+				ft_polish_parse(parse, j, i - 1);
+			else if (ret == 1 && parse->cmdsplit[j][i] == '\\' &&
+				parse->cmdsplit[j][i + 1] && parse->cmdsplit[j][i + 1] == '\"')
 				ft_polish_parse(parse, j, i - 1);
 			polisher(parse, i, j, ret, &in);
 		}
