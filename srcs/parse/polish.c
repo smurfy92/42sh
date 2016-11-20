a8ed92dca0ac10abdf786ee34c651db0aa32b045
Julien MONTIJANO <jmontija@e3r1p9.42.fr>
2016-11-20 21:45:37 +0100

    polish_fixing

--- srcs/parse/polish.c
+++ srcs/parse/polish.c
@@ -6,7 +6,7 @@
 /*   By: jmontija <jmontija@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/19 22:41:30 by jmontija          #+#    #+#             */
-/*   Updated: 2016/11/19 23:46:51 by jmontija         ###   ########.fr       */
+/*   Updated: 2016/11/20 21:43:48 by jmontija         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,29 @@
 ** deletes char in parse cmd at given indice
 */
 
-void		ft_polish_parse(t_parse *parse, int i)
+void		ft_polish_parse(t_parse *parse, int j, int i)
 {
-	parse->cmd[i + 1] = '\0';
-	if (parse->cmd[i + 2])
-		parse->cmd = JOINF(parse->cmd, SDUP(&parse->cmd[i + 2]), 3);
+	parse->cmdsplit[j][i + 1] = '\0';
+	if (parse->cmdsplit[j][i + 2])
+		parse->cmdsplit[j] = JOINF(parse->cmdsplit[j], SDUP(&parse->cmdsplit[j][i + 2]), 3);
 }
 
-void		polisher(t_parse *parse, int i, int ret, int *in)
+void		polisher(t_parse *parse, int i, int j, int ret, int *in)
 {
 	char	q;
 
 	if (*in == 0 && ret == 1)
 	{
-		q = parse->cmd[i];
-		ft_polish_parse(parse, i - 1);
-		while ((q == '\'') && (ret = check_parentheses(parse->cmd[i]))
-		== 1 && parse->cmd[i + 1] != '\'')
+		q = parse->cmdsplit[j][i];
+		ft_polish_parse(parse, j, i - 1);
+		while ((q == '\'') && (ret = check_parentheses(parse->cmdsplit[j][i]))
+		== 1 && parse->cmdsplit[j][i + 1] != '\'')
 			i++;
 		*in = 1;
 	}
 	else if (*in == 1 && ret == 0)
 	{
-		ft_polish_parse(parse, i - 1);
+		ft_polish_parse(parse, j, i - 1);
 		*in = 0;
 	}
 }
@@ -53,15 +53,20 @@ void		polish(t_parse *parse)
 	int		ret;
 	int		in;
 	int		i;
+	int		j;
 
-	i = -1;
 	in = 0;
+	j = -1;
 	check_parentheses(0);
-	while (parse->cmd[++i])
-	{
-		ret = check_parentheses(parse->cmd[i]);
-		if (parse->cmd[i] == '\\' && parse->cmd[i + 1])
-			ft_polish_parse(parse, i - 1);
-		polisher(parse, i, ret, &in);
+	while (parse->cmdsplit && parse->cmdsplit[++j])
+	{	
+		i = -1;
+		while (parse->cmdsplit[j][++i])
+		{
+			ret = check_parentheses(parse->cmdsplit[j][i]);
+			if (parse->cmdsplit[j][i] == '\\' && parse->cmdsplit[j][i + 1])
+				ft_polish_parse(parse, j, i - 1);
+			polisher(parse, i, j, ret, &in);
+		}
 	}
 }
