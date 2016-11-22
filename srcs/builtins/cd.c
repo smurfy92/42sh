8b9ce8651b42602cc7a12132be4dc50fabb21141
Victor DANAIN <vdanain@e2r9p6.42.fr>
2016-11-22 12:48:26 +0100

    cd 2 args & option

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: vdanain <vdanain@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/17 20:23:03 by julio             #+#    #+#             */
-/*   Updated: 2016/11/22 12:15:03 by vdanain          ###   ########.fr       */
+/*   Updated: 2016/11/22 12:42:31 by vdanain          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,37 @@ void	cderr_pwd(t_group *grp, char **path, struct stat s_buf, int opt)
 	else if (chdir((*path)) == 0)
 		update_pwd(grp, (*path), opt, curr_dir);
 	ft_strdel(path);
+}
+
+char	*starting_replace(char *path, char *replace, char *by)
+{
+	char	buff[2048];
+	char	tmp[2048];
+
+	ft_bzero(buff, 2048);
+	ft_bzero(tmp, 2048);
+	ft_strcpy(buff, path);
+	ft_strcpy(tmp, ft_strstr(buff, replace) + LEN(replace));
+	ft_bzero(ft_strstr(buff, replace), LEN(path));
+	ft_strcat(buff, by);
+	ft_strcat(buff, tmp);
+	return (ft_strdup(buff));
+}
+
+char	*replace_in_path(t_group *grp, t_parse *parse, int nb)
+{
+	char	*tmp;
+	char	buf[1024 + 1];
+
+	ft_bzero(buf, 1025);
+	tmp = RPW(grp, buf);
+	if (!ft_strstr(tmp, parse->cmdsplit[nb]))
+	{
+		error_cmd("Can't find in actual path", parse->cmdsplit[nb], 1);
+		return (NULL);
+	}
+	tmp = starting_replace(tmp, parse->cmdsplit[nb], parse->cmdsplit[nb + 1]);
+	return (tmp);
 }
 
 char	*return_home(t_group *grp, char **path)
@@ -69,8 +100,10 @@ int		update_path_cd(char **path, t_parse *parse, t_group *grp, int mode)
 	{
 		if (parse->cmdsplit[2] == NULL)
 			(*path) = return_home(grp, NULL);
-		else
+		else if (parse->cmdsplit[3] == NULL)
 			(*path) = SDUP(parse->cmdsplit[2]);
+		else
+			(*path) = replace_in_path(grp, parse, 2);
 		ret = (parse->cmdsplit[1][1] == 'P') ? 1 : 0;
 	}
 	return (ret);
@@ -83,36 +116,6 @@ int		is_param(char c)
 	return (0);
 }
 
-char	*starting_replace(char *path, char *replace, char *by)
-{
-	char	buff[2048];
-	char	tmp[2048];
-
-	ft_bzero(buff, 2048);
-	ft_bzero(tmp, 2048);
-	ft_strcpy(buff, path);
-	ft_strcpy(tmp, ft_strstr(buff, replace) + LEN(replace));
-	ft_bzero(ft_strstr(buff, replace), LEN(path));
-	ft_strcat(buff, by);
-	ft_strcat(buff, tmp);
-	return (ft_strdup(buff));
-}
-
-char	*replace_in_path(t_group *grp, t_parse *parse)
-{
-	char	*tmp;
-	char	buf[1024 + 1];
-
-	ft_bzero(buf, 1025);
-	tmp = RPW(grp, buf);
-	if (!ft_strstr(tmp, parse->cmdsplit[1]))
-	{
-		error_cmd("Can't find in actual path", parse->cmdsplit[1], 1);
-		return (NULL);
-	}
-	tmp = starting_replace(tmp, parse->cmdsplit[1], parse->cmdsplit[2]);
-	return (tmp);
-}
 
 int		builtin_cd(t_group *grp, t_parse *parse)
 {
@@ -125,12 +128,17 @@ int		builtin_cd(t_group *grp, t_parse *parse)
 	ft_bzero(buf, 1024);
 	if (parse->cmdsplit[1] == NULL)
 		path = return_home(grp, NULL);
+	else if ((parse->cmdsplit[1][0] != '-' && parse->cmdsplit[2] && parse->cmdsplit[3]) || (parse->cmdsplit[1][0] == '-' && parse->cmdsplit[2] && parse->cmdsplit[3] && parse->cmdsplit[4]))
+	{
+		error_cmd("Too many arguments", "cd", 1);
+		path = NULL;
+	}
 	else if (parse->cmdsplit[1][0] == '-' && parse->cmdsplit[1][1] == false)
 		update_path_cd(&path, parse, grp, 1);
 	else if (parse->cmdsplit[1][0] == '-' && is_param(parse->cmdsplit[1][1]))
 		opt = update_path_cd(&path, parse, grp, 2);
 	else if (parse->cmdsplit[1] && parse->cmdsplit[2])
-		path = replace_in_path(grp, parse);
+		path = replace_in_path(grp, parse, 1);
 	else if (ft_strcmp(parse->cmdsplit[1], "..") != 0)
 		path = SDUP(parse->cmdsplit[1]);
 	else
