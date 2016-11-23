b0b6724e2fc328219e6d13c49b3dd3586590fcaf
Smurfy <tranchierjules@gmail.com>
2016-11-23 17:34:52 +0100

    clean re

---
+++ clean.sh
@@ -0,0 +1,3 @@
+rm -rf `ls | grep -v "clean.sh" | grep -v "srcs" | grep -v "includes" | grep -v "Makefile"  | grep -v "README.md"\
+ | grep -v "tests.sh" | grep -v "TESTS" | grep -v "push.sh"`;
+rm **/**.*o;
